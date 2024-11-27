56432c933dbc7c7f94d017a485055d7dac537c44
Administrator <Administrator@N301PC26>
2024-11-27 16:05:14 +0700

    Add project files.

---
+++ Class Exercise 27_11_2024/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        string st = "";
+        Random rnd = new Random();
+        for (int i = 0; i < 1000; i++)
+        {
+            st += rnd.Next(500) + ",";
+        }
+        writeText("data.txt", st);
+
+        Console.WriteLine("noi dung file data.txt");
+        string content = readContent("data.txt");
+        Console.WriteLine(content);
+    }
+
+    static void writeText(string path, string content)
+    {
+        StreamWriter writer = new StreamWriter(path);
+        writer.WriteLine(content);
+        writer.Close();
+    }
+    static string readContent(string path)
+    {
+        StreamReader reader = new StreamReader(path);
+        string content = reader.ReadToEnd();
+        reader.Close();
+        return content;
+    }
+}
